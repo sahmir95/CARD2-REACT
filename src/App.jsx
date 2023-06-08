ad4f9b06f74d85d01fa11ab7c06c1a792cdad5fa
sahmir95 <sahar2200048@gmail.com>
2023-06-08 21:19:11 +0330

    fixed(change some things)

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ function App(){
     <Card 
     image="/images/carimg.jpg"
     title="Lizard"
-    desc="Chevrolet is an iconic American car brand known for its reliable
-    ,dependable,and affordable vehicles.Founded in 1911,Chevy has become one of 
+    desc="Chevrolet is an iconic American car brand known for its reliable, dependable, and 
+    affordable vehicles.Founded in 1911, Chevy has become one of 
     the most recognizable car brands in the world."
     />
   )
